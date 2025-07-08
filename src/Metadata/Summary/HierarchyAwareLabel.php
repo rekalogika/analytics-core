9b8a1bdd085d25d78627d6cc3ef2a590e26d0f50
root <1102197+priyadi@users.noreply.github.com>
2025-07-08 18:41:31 +0000

    chore: hierarchyaware label (#434)

--- src/Metadata/Summary/HierarchyAwareLabel.php
+++ src/Metadata/Summary/HierarchyAwareLabel.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
 namespace Rekalogika\Analytics\Metadata\Summary;
 
 use Rekalogika\Analytics\Common\Exception\LogicException;
-use Rekalogika\Analytics\Common\Util\HierarchicalTranslatableMessage;
+use Rekalogika\Analytics\Metadata\Util\HierarchicalTranslatableMessage;
 use Symfony\Contracts\Translation\TranslatableInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
